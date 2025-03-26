ab0b6b9c57698e3c00092fb80cc25818dc377b50
MhmmdSyahputra <putramhmmd22@gmail.com>
2025-03-26 13:57:05 +0700

    change env from vps and type import

--- src/pages/staffPages/configPage/academicYearPage/index.tsx
+++ src/pages/staffPages/configPage/academicYearPage/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HeaderTitlePage } from "../../../../components/headerTitlePage";
-import { TableAcademicYear } from "../../../../features/staffPages/configPage/academicYear/tableacademicYear";
+import { TableAcademicYear } from "../../../../features/staffPages/configPage/academicYear/tableAcademicYear";
 
 export const AcademicYearPage: React.FC = () => {
   
